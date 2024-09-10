02f445db860786c75486941976086007788a16ed
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-09-10 17:24:42 +0530

    Update pattern1.java

--- DSA/Pattern_Programming/pattern1.java
+++ DSA/Pattern_Programming/pattern1.java
@@ -42,10 +42,7 @@ public class Solution{
     public static void nForest(int n){
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(j>0){
-                    System.out.print(" ");
-                }
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
