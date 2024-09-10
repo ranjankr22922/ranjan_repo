ebba723b5c9d3a0f8bf98ec209cd4265114b6741
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-09-10 17:20:57 +0530

    Update pattern1.java

--- DSA/Pattern_Programming/pattern1.java
+++ DSA/Pattern_Programming/pattern1.java
@@ -37,9 +37,9 @@ Sample Output 3 :
   */
 
 import java.util.Scanner;
-public class Solution {
-    public static void nForest(int n) {
-        // Write your code here
+
+public class Solution{
+    public static void nForest(int n){
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(j>0){
@@ -51,9 +51,9 @@ public class Solution {
         }
     }
 
-public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int n=sc.nextInt();
-    nForest(n);
-}
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        nForest(n);
+    }
 }
