b22bb26a06f4d11717a66ea337bd65d4a5b23050
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-09-12 16:29:02 +0530

    Update reverseNum.java

--- DSA/Basics/reverseNum.java
+++ DSA/Basics/reverseNum.java
@@ -30,6 +30,10 @@ class Solution {
         int lastDigit = 0;
         while(x!=0){
             lastDigit = x%10;
+
+            if(revNum>Integer.MAX_VALUE/10 || revNum<Integer.MIN_VALUE/10){
+            return 0;
+        }
             revNum = (revNum*10) + lastDigit;
             x = x/10;
         }
