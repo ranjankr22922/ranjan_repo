57589f90dca26f235ca726c16d304bfc6c82e379
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-09-12 15:50:01 +0530

    Create countEvenDivide.java

---
+++ DSA/Basics/countEvenDivide.java
@@ -0,0 +1,45 @@
+/*
+Count Digits
+Difficulty: EasyAccuracy: 30.45%Submissions: 292K+Points: 2
+Given a number n. Count the number of digits in n which evenly divide n. Return an integer, total number of digits of n which divides n evenly.
+
+Note :- Evenly divides means whether n is divisible by a digit i.e. leaves a remainder 0 when divided.
+ 
+
+Examples :
+
+Input: n = 12
+Output: 2
+Explanation: 1, 2 when both divide 12 leaves remainder 0.
+Input: n = 2446
+Output: 1
+Explanation: Here among 2, 4, 6 only 2 divides 2446 evenly while 4 and 6 do not.
+Input: n = 23
+Output: 0
+Explanation: 2 and 3, none of them divide 23 evenly.
+Expected Time Complexity: O(n)
+Expected Space Complexity: O(1)
+ 
+
+Constraints:
+1<= n <=105
+*/
+
+class Solution {
+    public static int evenlyDivides(int n) {
+        int count = 0;
+        int original = n;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            n = n / 10;
+            
+            // Avoid division by zero
+            if (digit != 0 && original % digit == 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
