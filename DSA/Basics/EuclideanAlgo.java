52d650ca2c1c42eb1c9f6c7c06d66b2e8eda3f43
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-09-20 11:56:25 +0530

    Update EuclideanAlgo.java

--- DSA/Basics/EuclideanAlgo.java
+++ DSA/Basics/EuclideanAlgo.java
@@ -1,1 +1,26 @@
+// gcd(a,b) = gcd(a-b, b) where a>b
 
+import java.util.Scanner;
+
+class pattern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while (a>0 && b>0) {
+            if(a>b){
+                a=a%b;
+            }
+            if(b>a){
+                b=b%a;
+            }
+        }
+        if(a==0){
+            System.out.println(b);
+        }else{
+            System.out.println(a);
+        }
+        sc.close();
+    }
+}
