0b12083075f242f463668b9ea75fc7479b66bcdb
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-10-10 18:04:04 +0530

    Update basicRecursion.java

--- DSA/Basics/Recursion/basicRecursion.java
+++ DSA/Basics/Recursion/basicRecursion.java
@@ -8,4 +8,18 @@ class HelloWorld {
         hi();
     }
 }
+
 //Segmentation Error - Stack Overflow(out of memory)
+
+class HelloWorld {
+    static int count = 0;
+    public static void hi(){
+        System.out.println(count);
+        count++;
+        hi();
+    }
+    
+    public static void main(String[] args) {
+        hi();
+    }
+}
